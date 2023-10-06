a6266a6274fe66f0bc1049b203abcab0029ec119
МаксимусПрайм <130079536+ofevan78@users.noreply.github.com>
2023-10-06 19:38:46 +0300

    добавил метод ввода массива с терминала

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+//длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+//При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+
+string[] Main(string array1)
+{
+    string[] array = new string[5];
+    {
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = Console.ReadLine();
+
+        }
+    }
+
+    return array;
+}
